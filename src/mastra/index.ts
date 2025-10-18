7ff07363f33f50d974eae4e6dfa6599889daa3c4
Puvish <p.kungsapattarakul@gmail.com>
2025-10-18 16:53:58 +0700

    init

---
+++ src/mastra/index.ts
@@ -0,0 +1,48 @@
+import { Mastra } from "@mastra/core/mastra";
+import { PinoLogger } from "@mastra/loggers";
+import { LibSQLStore } from "@mastra/libsql";
+import { weatherWorkflow } from "./workflows/weather-workflow";
+import { weatherAgent } from "./agents/weather-agent";
+
+export const mastra = new Mastra({
+  workflows: { weatherWorkflow },
+  agents: { weatherAgent },
+  storage: new LibSQLStore({
+    // stores observability, scores, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: "Mastra",
+    level: "info",
+  }),
+  telemetry: {
+    // Telemetry is deprecated and will be removed in the Nov 4th release
+    enabled: false,
+  },
+  observability: {
+    // Enables DefaultExporter and CloudExporter for AI tracing
+    default: { enabled: true },
+  },
+  server: {
+    middleware: [
+      {
+        handler: async (c, next) => {
+          if (process.env.environment !== "DEVELOPMENT") {
+            const authHeader = c.req.header("x-mastra-api-key");
+            const isAuthenticated = authHeader == process.env.MASTRA_API_KEY;
+            if (!authHeader || !isAuthenticated) {
+              return new Response("Unauthorized", { status: 401 });
+            }
+          }
+          await next();
+        },
+        path: "/api/*",
+      },
+      // Add a global request logger
+      async (c, next) => {
+        console.log(`${c.req.method} ${c.req.url}`);
+        await next();
+      },
+    ],
+  },
+});
